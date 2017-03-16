80229278c73a5b6915a7903b2321f3c92c7ec155
stephen <skingsto@in.ibm.com>
2017-03-16 21:16:11 +0530

    commiting local serverapi changes

---
+++ demo/ViewController/MoreOptionsTableViewController.swift
@@ -0,0 +1,58 @@
+//
+//  MoreOptionsViewControllerTableViewController.swift
+//  ChurchTemplate
+//
+//  Created by stephen sam kingston on 3/2/17.
+//  Copyright © 2017 stephen sam kingston. All rights reserved.
+//
+
+import UIKit
+
+protocol TempStatus{
+func selectedOptions(temperature:Temperature)
+}
+
+class MoreOptionsTableViewController: UITableViewController {
+
+    var delTempStatus:TempStatus?
+    var optionsArray:[Temperature]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.preferredContentSize = CGSize(width: 270, height: 427)
+        tableView.register(UITableViewCell.classForKeyedArchiver(),
+                           forCellReuseIdentifier: "reuseIdentifier")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return optionsArray!.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+       cell.backgroundColor =  UIColor.black
+       cell.textLabel?.text = optionsArray?[indexPath.row].temperature
+       cell.textLabel?.textColor = UIColor.white
+       return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        delTempStatus?.selectedOptions(temperature: (optionsArray?[indexPath.row])!)
+    }
+
+}
--- demo/ViewController/TemperatureViewController.swift
+++ demo/ViewController/TemperatureViewController.swift
@@ -9,20 +9,25 @@
 import UIKit
 import SocketIO
 
-class TemperatureViewController
-: UIViewController {
+class TemperatureViewController: UIViewController,UIPopoverPresentationControllerDelegate {
 
+internal var temperatureStack:[Temperature] = []
+    
+internal var popOver:MoreOptionsTableViewController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         retrievePodcast()
+        
     }
 
     
     func retrievePodcast(){
  
-        DataRetriever.get(requestType: .Temperature) { (tracks: [Temperature]) in
+        DataRetriever.get(requestType: .Temperature) { (temperature: [Temperature]) in
           DispatchQueue.main.async {
-              print(tracks)
+              print(temperature)
+            self.temperatureStack = temperature
             }
           }
         }
@@ -32,6 +37,35 @@ class TemperatureViewController
         // Dispose of any resources that can be recreated.
     }
 
+   
+    @IBAction func btnTapped(_ sender: Any) {
+        popOver = MoreOptionsTableViewController()
+        popOver?.delTempStatus = self as? TempStatus
+        popOver?.modalPresentationStyle = UIModalPresentationStyle.popover
+        popOver?.optionsArray = self.temperatureStack
+        // set up the popover presentation controller
+        popOver?.popoverPresentationController?.permittedArrowDirections = UIPopoverArrowDirection.any
+        if let pop = popOver?.popoverPresentationController {
+             pop.delegate = self
+        }
+       
+        popOver?.popoverPresentationController?.sourceView = sender as? UIView // button
+        
+        popOver?.popoverPresentationController?.sourceRect = (sender as AnyObject).bounds
+        self.present(popOver!, animated: true, completion:nil)
+    }
+    func adaptivePresentationStyle(for controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
+        return UIModalPresentationStyle.none
+    }
 
+}
+
+
+
+
+extension TemperatureViewController{
+    func selectedOptions(temperature:Temperature){
+     print("I am here ,Not an issue")
+    }
 }
 
